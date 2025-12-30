43d3a08f39a18c57f1adef0e9901cfc408fe745c
Mohamad <M.Beheshti148@gmail.com>
2025-12-30 22:10:10 +0330

    Adding related products to product details and sorting templates

--- polls/templatetags/show_product.py
+++ polls/templatetags/show_product.py
@@ -9,3 +9,11 @@ def show_latest_product():
 
     return {"latest_products": latest_products}
 
+@register.inclusion_tag('product/includes/similar_product.html')
+def show_similar_product(product: Product):
+    categories = product.category.all()
+    similar_products = Product.objects.filter(status=ProductStatusType.active, category__in=categories).exclude(pk=product.pk).order_by("-created_date")[:4]
+
+    return {"similar_products": similar_products}
+
+
