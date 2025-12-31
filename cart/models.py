58e9fe2e5473cad76834b300680665f9c1d38e7a
Mohamad <M.Beheshti148@gmail.com>
2025-12-31 13:28:08 +0330

    Create cart models

--- cart/models.py
+++ cart/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from accounts.models import User
+from product.models import Product
 
-# Create your models here.
+
+class Cart(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = 'cart'
+
+class CartItem(models.Model):
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.IntegerField()
+
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = 'cart_item'
